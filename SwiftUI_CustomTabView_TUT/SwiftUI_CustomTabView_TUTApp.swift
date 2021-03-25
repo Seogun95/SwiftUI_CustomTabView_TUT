4d771b05b0a998081d7d16c851b947c4a0a3385c
SeonJungKim <ksj083895@gmail.com>
2021-03-25 11:11:56 +0500

    Initial Commit

---
+++ SwiftUI_CustomTabView_TUT/SwiftUI_CustomTabView_TUTApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CustomTabView_TUTApp.swift
+//  SwiftUI_CustomTabView_TUT
+//
+//  Created by 김선중 on 2021/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CustomTabView_TUTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
